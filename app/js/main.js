2d0454d29ae3a1de82f456e2240dd3071ef4950e
html3012 <b_dana@ukr.net>
2021-09-05 23:01:10 +0300

    added header, footer, hero

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,21 @@
 $(function () {
-   
+   $('.btn-burger').on('click', function() {
+    $('.btn-burger__line').toggleClass('btn-burger__line--active');
+    $('.header__menu').toggleClass('header__manu--active');
+    $('body').toggleClass('lock');
+   });
+
+   let hero = $('.hero'),
+      scrollPos = $(window).scrollTop(),
+      heroH = hero.innerHeight();
+
+   $(window).on('scroll load resize', function () {
+      heroH = hero.innerHeight();
+      scrollPos = $(this).scrollTop();
+   if(scrollPos > heroH - 300) {
+      $('.header').addClass('header--fixed');
+      } else {
+         $('.header').removeClass('header--fixed');
+      }
+   });
 });
